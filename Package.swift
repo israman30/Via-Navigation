02d64880987e69edce820e71faaaa0059a747810
Israel Manzo <israelmanzo814@gmail.com>
2026-05-16 21:43:26 -0400

    update version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -29,7 +29,6 @@ let package = Package(
             dependencies: ["Via"],
             path: "Via/Examples"
         ),
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
 
